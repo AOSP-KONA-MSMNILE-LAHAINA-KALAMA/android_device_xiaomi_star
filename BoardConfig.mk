b665c725e5eb623ff20b43dcad2c3ad26ef675a8
Cosmin Tanislav <demonsingur@gmail.com>
2021-08-29 14:15:40 +0300

    mars: add modules load list

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,10 @@ TARGET_BOOTLOADER_BOARD_NAME := mars
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/mars_defconfig
 
+# Kernel modules
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
+
 # Partitions
 BOARD_DTBOIMG_PARTITION_SIZE := 25165824
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 114001162240
