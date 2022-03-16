d0124696f3c889852f9eb50ed5b762d8ce4c5918
EcrosoftXiao <xy19990903@gmail.com>
2022-03-16 23:33:00 +0800

    mars: Drop modules.load.recovery

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,6 @@ TARGET_KERNEL_CONFIG := vendor/mars_defconfig
 
 # Kernel modules
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
-BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
 
 # Partitions
 BOARD_DTBOIMG_PARTITION_SIZE := 25165824
