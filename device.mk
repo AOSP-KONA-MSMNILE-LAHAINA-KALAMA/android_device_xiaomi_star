62d6f9540d26c40ed66ddbcda7011fe57dcf59a9
LuK1337 <priv.luk@gmail.com>
2022-03-24 21:56:15 -0300

    mars: overlay: Set default Settings.Global.DEVICE_NAME according to MIUI
    MIUI uses ro.product.marketname for default device name.

--- device.mk
+++ device.mk
@@ -30,6 +30,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-pe
 
 PRODUCT_PACKAGES += \
     FrameworksResVenus \
+    SettingsProviderOverlayMars \
     SettingsResVenus \
     SystemUIResVenus
 
