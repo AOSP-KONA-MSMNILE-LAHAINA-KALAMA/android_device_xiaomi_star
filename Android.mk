1412d01759ca926d7637878b2b1fc8d1145700bf
Akash Srivastava <akashniki@gmail.com>
2022-04-10 01:10:22 +0530

    mars: Drop other device support
    * This tree will be used for star and mars only

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter haydn lisa mars odin renoir venus,$(TARGET_DEVICE)),)
+ifneq ($(filter mars star,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
