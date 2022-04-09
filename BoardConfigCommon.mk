acdfda100fb98ee68c0ca6224afefc16674599c1
Akash Srivastava <akashniki@gmail.com>
2022-04-10 00:10:09 +0530

    mars: Drop SOONG_NAMESPACES
    * Not needed for us

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -206,14 +206,6 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
-# Soong
-SOONG_CONFIG_NAMESPACES += xiaomiSm8350Vars
-SOONG_CONFIG_xiaomiSm8350Vars += \
-    fingerprint_use_extension \
-    vibrator_use_effect_stream
-SOONG_CONFIG_xiaomiSm8350Vars_fingerprint_use_extension ?= false
-SOONG_CONFIG_xiaomiSm8350Vars_vibrator_use_effect_stream ?= false
-
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
