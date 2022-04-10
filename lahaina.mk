a84311e7c5248dc29ab4582bf2404b6e4bff61ac
Akash Srivastava <akashniki@gmail.com>
2022-04-11 02:40:44 +0530

    star: Drop bootanimation flags
    * This is useless for us

--- lahaina.mk
+++ lahaina.mk
@@ -106,10 +106,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2400
-TARGET_SCREEN_WIDTH := 1080
-
 # Boot control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl-qti \
