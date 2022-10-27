d68dbf7a830483f75c8bd92453d5331a9d45002e
Akash Srivastava <akashniki@gmail.com>
2022-10-27 15:12:20 +0700

    star: Add System blobs for CAF's Perf HAL
    * I ported CAF's Frawework boost to AOSP, but for that to work properly these libs are needed

--- lahaina.mk
+++ lahaina.mk
@@ -349,6 +349,16 @@ TARGET_BOARD_PLATFORM := lahaina
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Perf
+PRODUCT_PACKAGES += \
+    libpsi.vendor \
+    libtflite
+
+# Perf Jars
+PRODUCT_BOOT_JARS += \
+    QPerformance \
+    UxPerformance
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
@@ -430,6 +440,7 @@ PRODUCT_COPY_FILES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0 \
+    android.hardware.thermal@2.0.vendor \
     android.hardware.thermal@2.0-service.qti
 
 # Touchscreen
