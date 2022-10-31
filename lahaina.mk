ceb0113efa03c34e275f6dde628cf4f7ad4c626b
Akash Srivastava <akashniki@gmail.com>
2022-11-01 00:33:05 +0700

    star: Switch back to HIDL bluetooth HAL

--- lahaina.mk
+++ lahaina.mk
@@ -83,19 +83,6 @@ PRODUCT_PACKAGES += \
     otapreopt_script
 
 # Bluetooth
-PRODUCT_PACKAGES += \
-    audio.bluetooth.default \
-    libbthost_if
-
-PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.1.vendor \
-    android.hardware.bluetooth.audio-impl \
-    com.dsi.ant@1.0.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
-
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/audio-hal/primary-hal/configs/common/bluetooth_qti_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_qti_audio_policy_configuration.xml
 
