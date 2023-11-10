bb958489bf4c2cb6981220ce5660d1a3289b1137
LuK1337 <priv.luk@gmail.com>
2023-11-10 15:00:36 +0100

    star: Build android.frameworks.sensorservice@1.0.vendor
    Required by mm-pp-deps.
    
    Change-Id: Iaf821dcbb07d7820f2fc3ff5ca9cca5e973360f2

--- lahaina.mk
+++ lahaina.mk
@@ -308,6 +308,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@2.1-service.xiaomi-multihal
 
 PRODUCT_COPY_FILES += \
