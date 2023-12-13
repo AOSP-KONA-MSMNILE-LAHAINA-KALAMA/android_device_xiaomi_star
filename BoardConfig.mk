1065c435323450fef899fd9c3ba1a69bc25d84ad
Akash Srivastava <akashniki@gmail.com>
2023-12-13 12:13:25 +0100

    star: Drop VENDOR_SECURITY_PATCH

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -201,9 +201,6 @@ TARGET_USERIMAGES_USE_F2FS := true
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
 
-# Security patch level
-VENDOR_SECURITY_PATCH := 2023-05-01
-
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
