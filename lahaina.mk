60f21b4fe94ef32b12a28cb27fb2c803b87f6353
Akash Srivastava <akashniki@gmail.com>
2024-01-25 16:50:32 +0100

    Revert "star: Switch to prebuilt camera stack"
    This reverts commit 97b197ea396bc9617fb53d9eb4437e41b02e3223.

--- lahaina.mk
+++ lahaina.mk
@@ -102,7 +102,13 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Camera2
+    Camera2 \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
+    libgui_vendor \
+    libcamera2ndk_vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
