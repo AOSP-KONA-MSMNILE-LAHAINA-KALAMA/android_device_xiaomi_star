48fcbca5733147b96fa0877b38991191e0d00545
Akash Srivastava <akashniki@gmail.com>
2024-01-26 14:53:51 +0100

    star: Shim another missing symbol for vendor.ozoaudio.media.c2@1.0-service
    * CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.ozoaudio.media.c2@1.0-service": cannot locate symbol "_ZN17C2PooledBlockPoolC1ERKNSt3__110shared_ptrI11C2AllocatorEEm" referenced by "/vendor/lib64/libozoc2store.so"...

--- libshims/lib-ozoaudio.cpp
+++ libshims/lib-ozoaudio.cpp
@@ -1,3 +1,4 @@
 extern "C" {
    void _ZN27C2PlatformStorePluginLoader15createAllocatorEjPNSt3__110shared_ptrI11C2AllocatorEE() {}
+   void _ZN17C2PooledBlockPoolC1ERKNSt3__110shared_ptrI11C2AllocatorEEm() {}
 }
