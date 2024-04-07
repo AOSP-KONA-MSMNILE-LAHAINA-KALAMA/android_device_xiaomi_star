c9fe447ccde9387ebfcc8ba42bac262faedf2993
Akash Srivastava <akashniki@gmail.com>
2024-04-07 11:31:44 +0200

    star: Switch to udfps handler lib
    Co-authored-by: Arian <arian.kulmer@web.de>
    Change-Id: Ib837e4a70015dc24d431458a1cce5b86e87816a9

--- lahaina.mk
+++ lahaina.mk
@@ -143,7 +143,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3.vendor \
-    android.hardware.biometrics.fingerprint@2.3-service.star \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    libudfpshandler \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
 PRODUCT_COPY_FILES += \
